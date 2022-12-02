1a197289fa1f3dd679e57faa7a7b05b5403901e0
alex <alex@gg.com>
2022-12-02 11:29:08 +0300

    first commit

--- app/src/main/java/com/example/androidstudiodialog/MainActivity.java
+++ app/src/main/java/com/example/androidstudiodialog/MainActivity.java
@@ -16,29 +16,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onCloseButtonClick(View view) {
+        String[] mIngredients = {"Томаты","Курица","Грибы","Маслины"};
+        final boolean[] mSelectedIngredients = { false, false, false, false};
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("Выход из приложения")
-                .setMessage("Вы уверены что хотите закрыть приложение?")
+        builder.setTitle("Выберите ингредиенты для пиццы")
                 .setCancelable(false)
+                .setMultiChoiceItems(mIngredients, null, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        mSelectedIngredients[which] = isChecked;
+                    }
+                })
                 .setPositiveButton("ОК",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                finish();
+                        new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int id){
+
                             }
                         })
                 .setNegativeButton("Отмена",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+//        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+//        builder.setTitle("Выход из приложения")
+//                .setMessage("Вы уверены что хотите закрыть приложение?")
+//                .setCancelable(false)
+//                .setPositiveButton("ОК",
+//                        new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int id) {
+//                                finish();
+//                            }
+//                        })
+//                .setNegativeButton("Отмена",
+//                        new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int id) {
+//                                dialog.cancel();
+//                            }
+//                        });
         AlertDialog alert = builder.create();
         alert.show();
     }
 }
-//    public void onCloseButtonClick1(View view){
-//        String[] mIngredients = {"Томаты","Курица","Грибы","Маслины"};
-//        final boolean[] mSelectedIngredients = { false, false, false, false};
+//    public void onCloseButtonClick(View view){
+//       String[] mIngredients = {"Томаты","Курица","Грибы","Маслины"};
+//       final boolean[] mSelectedIngredients = { false, false, false, false};
 //        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 //        builder.setTitle("Выберите ингредиенты для пиццы")
 //                .setCancelable(false)
@@ -52,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
 //                        new DialogInterface.OnClickListener(){
 //                    public void onClick(DialogInterface dialog, int id){
 //
-//                    }
+//                   }
 //                        });
 //                }
-//    }
+//   }
